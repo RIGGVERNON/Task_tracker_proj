eafb71b3798b21366a3185e472d0cabe589756fa
Rigg Desouza <72122433+RIGGVERNON@users.noreply.github.com>
2023-09-04 19:23:27 +0530

    firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -5,12 +5,7 @@ import 'firebase/auth';
 import 'firebase/firestore';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCSYY0GrPaeNOqoR6Sm10H2nAFSxRIIMUk",
-    authDomain: "task-tracker-598ea.firebaseapp.com",
-    projectId: "task-tracker-598ea",
-    storageBucket: "task-tracker-598ea.appspot.com",
-    messagingSenderId: "697904664205",
-    appId: "1:697904664205:web:35c3bd9a78764dc682dcc4"
+    
 };
 
 firebase.initializeApp(firebaseConfig);
